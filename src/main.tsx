b09ba15a85610314d1942968db3b90969a158d26
jsf <azimut.nt@gmail.com>
2024-04-05 18:19:22 +0700

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,11 @@ import ReactDOM from 'react-dom/client';
 import { CssBaseline } from '@mui/material';
 import { Theme } from './Theme.tsx';
 import { App } from './App';
-import { SWRConfig, SWRConfiguration } from 'swr';
-
-const swrConfig: SWRConfiguration = {
-    fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
-};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Theme>
         <CssBaseline>
-            <SWRConfig value={swrConfig}>
-                <App />
-            </SWRConfig>
+            <App />
         </CssBaseline>
     </Theme>,
 );
